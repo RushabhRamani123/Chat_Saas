1dc358eede42d567a92bf3734293280e19e1d06c
rushabh ramani <rushabhramani16@gmail.com>
2024-07-17 22:11:07 +0530

    Emoji-Picker Bug fixed

--- src/components/EmojiPicker.jsx
+++ src/components/EmojiPicker.jsx
@@ -1,60 +1,73 @@
 /* eslint-disable react/prop-types */
-import { ClickAwayListener } from '@mui/base';
-import { useState } from 'react';
-import { useTheme,styled } from "@mui/material/styles";
-import { Picker } from 'emoji-mart';
-import data from '@emoji-mart/data'
-import { Box, IconButton } from '@mui/material';
-import { Smiley } from 'phosphor-react';
+import { ClickAwayListener } from "@mui/base";
+import { useState } from "react";
+import { useTheme, styled } from "@mui/material/styles";
+import Picker from "@emoji-mart/react";
+import data from "@emoji-mart/data";
+import { Box, IconButton } from "@mui/material";
+import { Smiley } from "phosphor-react";
 const RootStyle = styled(Box)({
-  position:'relative'
-})
-const PickerStyle = styled('div')(({
+  position: "relative",
+});
+const PickerStyle = styled("div")({
   bottom: 40,
-  overflow: 'hidden',
-  position:'absolute',
-}))
+  overflow: "hidden",
+  position: "absolute",
+});
+
 const EmojiPicker = ({
   // eslint-disable-next-line no-unused-vars
-  disabled, 
-    value, 
+  disabled,
+  value,
+  // eslint-disable-next-line no-unused-vars
+  setValue,
+  // eslint-disable-next-line no-unused-vars
+  alignRight = false,
+  ...other
+}) => {
+  const [emojiPickerState, setEmojiPickerState] = useState(false);
+  const theme = useTheme();
+  let emojiPicker;
+  if (emojiPickerState) {
     // eslint-disable-next-line no-unused-vars
-    setValue,
-    // eslint-disable-next-line no-unused-vars
-    alignRight=false,
-    ...other}) => {
-    const [emojiPickerState, setEmojiPickerState] = useState(false);
-    const theme = useTheme(); 
-    let emojiPicker; 
-    if (emojiPickerState) {
-        // eslint-disable-next-line no-unused-vars
-        emojiPicker = <Picker data={data} color={theme.palette.primary.main} title="Pick your emoji..." onSelect={(emoji)=>{value+emoji ?.native}} /> 
-    }
-    const triggerPicker = (e) => {
-        e.preventDefault(); 
-        setEmojiPickerState(true); 
-    }
-    const handleClickAway = () => {
-        setEmojiPickerState(false); 
-    }
+    emojiPicker = (
+      <Picker
+        data={data}
+        color={theme.palette.primary.main}
+        title="Pick your emoji..."
+        onSelect={(emoji) => {
+          setValue(value + emoji?.native)
+        }}
+
+      />
+    );
+  }
+  const triggerPicker = (e) => {
+    e.preventDefault();
+    setEmojiPickerState(true);
+  };
+  const handleClickAway = () => {
+    setEmojiPickerState(false);
+  };
   return (
-      <ClickAwayListener onClickAway={handleClickAway}>
+    <ClickAwayListener onClickAway={handleClickAway}>
       <RootStyle {...other}>
         <PickerStyle
           sx={{
             ...(alignRight && {
-              right: '-2',
-              left:'auto !important'
-        })}}
+              right: "-2",
+              left: "auto !important",
+            }),
+          }}
         >
           {emojiPicker}
         </PickerStyle>
-        <IconButton disabled={disabled} size='small' onClick={triggerPicker}>
-          <Smiley /> 
+        <IconButton disabled={disabled} size="small" onClick={triggerPicker}>
+          <Smiley />
         </IconButton>
-          </RootStyle>
-    </ClickAwayListener> 
-  )
-}
+      </RootStyle>
+    </ClickAwayListener>
+  );
+};
 
-export default EmojiPicker
+export default EmojiPicker;
