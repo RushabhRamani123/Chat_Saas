48b7e4f7299617397b3cd96ca5509d8266e1983e
rushabh ramani <rushabhramani16@gmail.com>
2024-07-23 23:59:25 +0530

    Login ui create

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,14 +1,28 @@
-import { Box, Card, CardContent, Container, TextField, Typography } from "@mui/material"
+import { Box, Button, Card, CardContent, Container, Link, Stack, TextField, Typography } from "@mui/material"
 const Login = () => {
   return (
     <Box>
-        <Container>
+        <Container maxWidth='sm'>
         <Card>
             <CardContent>
-                <Typography>Login</Typography>
-                <Typography>Login</Typography>
-                <TextField label="email" placeholder="Enter your email"/>
-                <TextField label="password" placeholder="Enter your password"/>
+                <Stack spacing={2}>
+                <Typography variant="h5">Login</Typography>
+                <Typography variant="subtittle1">Enter your Email below to login into account</Typography>
+                </Stack>
+               <Stack spacing={2}>
+               <TextField fullWidth='false' label="email" placeholder="Enter your email"/>
+               <TextField fullWidth='false' label="password" placeholder="Enter your password"/>
+                <Button fullWidth variant="contained">Login</Button>
+                <Button fullWidth variant="outlined">Login with Google</Button>
+               </Stack>
+                <Stack >
+                  <Typography>
+                    Don`&apos`t have an account? 
+                  </Typography>
+                  <Link heref='#'>
+                  Singin
+                  </Link>
+                </Stack>
             </CardContent>
         </Card>
         </Container>
